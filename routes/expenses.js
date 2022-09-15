4e11252ef908959dfa2c0d7fa0e7d579e7f2a453
DLindeblom <drdlindeblom@gmail.com>
2022-09-14 20:12:05 -0400

    add post route for adding expenses

--- routes/expenses.js
+++ routes/expenses.js
@@ -5,11 +5,24 @@ const router = require('express').Router()
 
 module.exports = (db) => {
 
-  router.get('/', function(req, res, next) {
+  router.get('/', (req, res) => {
     db.query('SELECT * FROM expenses').then(data => {
       res.json(data.rows)
     });
   });
+
+  router.post('/', (req, res) => { //add expense
+    const queryString = `
+      INSERT INTO expenses
+      (budget_id, description, date, amount)
+      VALUES ($1, $2, $3, $4)`
+
+    const queryParams = [req.body.budgetId, req.body.description, req.body.date, req.body.amount]
+
+    db.query(queryString, queryParams).then(data => {
+      res.json(data)
+    })
+  })
 
   return router
 }
