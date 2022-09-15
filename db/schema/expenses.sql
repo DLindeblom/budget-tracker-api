3533f6a2e8bda8720528bf57a6333e1af5fe15df
DLindeblom <drdlindeblom@gmail.com>
2022-09-15 10:43:26 -0400

    change amount value to include decimals

--- db/schema/expenses.sql
+++ db/schema/expenses.sql
@@ -1,9 +1,9 @@
-DROP TABLE if EXISTS;
+DROP TABLE IF EXISTS expenses;
 
 CREATE TABLE expenses (
   id SERIAL PRIMARY KEY NOT NULL,
   budget_id INTEGER REFERENCES budgets (id) NOT NULL,
   description VARCHAR(25) NOT NULL,
   date VARCHAR NOT NULL,
-  amount INTEGER NOT NULL
+  amount DECIMAL(6,2) NOT NULL
 );
